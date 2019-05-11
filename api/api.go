fb09eb8e901cdb81632c77786e2cc9437aafc8ed
Jose <jsfuentescastillo@gmail.com>
2019-05-11 11:38:24 +0200

    Gen server

--- api/api.go
+++ api/api.go
@@ -1,4 +1,4 @@
-//go:generate ../scripts/swagger generate server -A payments -f ./swagger.yml
+//go:generate ../scripts/swagger generate server --name=payments --spec=./swagger.yml --target=../pkg --exclude-main
 
 //Package payments provides interfaces related to payments
 package payments
