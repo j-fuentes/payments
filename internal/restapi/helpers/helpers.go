83614b27f75e5c3cdde011c8112bca8ef15da7a3
Jose <jsfuentescastillo@gmail.com>
2019-05-15 02:19:43 +0200

    Added some validation

--- internal/restapi/helpers/helpers.go
+++ internal/restapi/helpers/helpers.go
@@ -1,22 +1,37 @@
 package helpers
 
-import "net/http"
+import (
+	"net/http"
 
-// Marshable is something that can be rendered into JSON accoring to the schema.
+	"github.com/go-openapi/strfmt"
+	"github.com/golang/glog"
+	"github.com/juju/errors"
+)
+
+// Marshable is something that can be rendered into JSON according to the schema.
 type Marshable interface {
 	MarshalBinary() ([]byte, error)
+	Validate(formats strfmt.Registry) error
 }
 
 // WriteRes writes in an http response a Marshable
 func WriteRes(w http.ResponseWriter, m Marshable) error {
+	err := m.Validate(nil)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return errors.BadRequestf("%+v", err)
+	}
+
 	bb, err := m.MarshalBinary()
 	if err != nil {
+		glog.Errorf("Cannot marshall object: %+v", err)
+		w.WriteHeader(http.StatusInternalServerError)
 		return err
 	}
 
 	_, err = w.Write(bb)
 	if err != nil {
-		return err
+		panic(err)
 	}
 
 	return nil
