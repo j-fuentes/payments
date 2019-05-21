8ab8b3dbc91380675c25835b391fe9383aeb98cb
Jose <jsfuentescastillo@gmail.com>
2019-05-21 21:53:52 +0200

    Test GET /payments

---
+++ internal/restapi/payments_test.go
@@ -0,0 +1,53 @@
+package restapi
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"github.com/j-fuentes/payments/internal/store"
+	"github.com/j-fuentes/payments/pkg/models"
+)
+
+func TestGetPayments(t *testing.T) {
+	genDesc := "hello world"
+	payments := []*models.Payment{
+		&models.Payment{ID: 1, Description: &genDesc},
+		&models.Payment{ID: 2, Description: &genDesc},
+	}
+
+	sv := NewPaymentsServer(store.NewVolatilePaymentsStore(payments))
+
+	t.Run("returns a valid list of payments", func(t *testing.T) {
+		req, err := http.NewRequest("GET", "/payments", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(sv.GetPayments)
+
+		handler.ServeHTTP(rr, req)
+
+		if got, want := rr.Code, http.StatusOK; got != want {
+			t.Errorf("handler returned wrong status code. got: %d, want: %d", got, want)
+		}
+
+		var res models.Payments
+		err = res.UnmarshalBinary(rr.Body.Bytes())
+		if err != nil {
+			t.Errorf("Cannot unmarshal response: %+v", err)
+		}
+		err = res.Validate(nil)
+		if err != nil {
+			t.Errorf("Validation failed for response: %+v", err)
+		}
+
+		if got, want := res.Data, payments; !reflect.DeepEqual(got, want) {
+			t.Errorf("Data does not match. got: %+v, want: %+v", got, want)
+		}
+
+		// TODO: Validate Link once we define what it should be
+	})
+}
